1984e065446b99ba993efbf337871a2239aa04dd
luojiarui <luojiarui2@163.com>
2021-01-06 17:20:59 +0800

    and Calculate code

--- read_config/__init__.py
+++ read_config/__init__.py
@@ -29,15 +29,19 @@ class InitParas(QThread):
         init_parameters = con.items('parameters')  # 返回结果为元组
         # 可以通过dict方法转换为字典
         init_parameters = dict(init_parameters)
-        self.signal.emit([init_parameters])  # 发射信号
+        # 获取参数对应关系
+        paras_map = con.items('paras_map')
+        paras_map = dict(paras_map)
+        self.signal.emit([init_parameters, paras_map])  # 发射信号
 
 class WriteConfig(QThread):
     signal = pyqtSignal(list)  # 括号里填写信号传递的参数
 
-    def __init__(self, args_list, paras):
+    def __init__(self, args_list, paras, paras_map):
         super().__init__()
         self.config_file = args_list
         self.paras = paras
+        self.paras_map = paras_map
 
     def __del__(self):
         self.wait()
@@ -50,8 +54,13 @@ class WriteConfig(QThread):
         # con.add_section("parameters")
         for key in self.paras:
             con.set("parameters", key, self.paras[key])
+
+        # 写入参数对应关系
+        for key in self.paras_map:
+            con.set("paras_map", key, self.paras_map[key])
+
         try:
-            with open(self.config_file, "w+") as f:
+            with open(self.config_file, "w+", encoding='utf-8') as f:
                 con.write(f)
         except ImportError:
             self.signal.emit([])  # 发射空信号
--- window.py
+++ window.py
@@ -5,11 +5,11 @@ import sys
 
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QDialog, QFileDialog, QTableWidgetItem
-
 from calculator import Calculate
 from read_config import InitParas, WriteConfig
 
 CONFIG = 'config.ini'
+
 
 class test1(QMainWindow):
     def __init__(self):
@@ -24,6 +24,8 @@ class test1(QMainWindow):
         self.init_config_thread.start()  # 启动线程
 
         self.parameters = {}
+        self.paras_map = None
+        self.clg_file = None
 
     # 读取保存参数的回调函数
     def init_paras_callback(self, value):
@@ -32,6 +34,8 @@ class test1(QMainWindow):
             self.paras_keys = [x for x in parameters]
             for key in parameters:
                 eval('self.' + key).setPlainText(parameters[key])
+
+            self.paras_map = value[1]  # 取到参数对应关系
         else:
             pass
 
@@ -39,29 +43,60 @@ class test1(QMainWindow):
     def file_dialog(self):
         # 设置文件扩展名过滤,注意用双分号间隔
         self.clg_file, filetype = QFileDialog.getOpenFileName(self, "选取文件", get_desk_path(),
-                                                              "Text Files (*.clg);;All Files (*)")
+                                                              "Text Files (*.txt);;All Files (*)")
         if self.clg_file:
-            self.output_file = self.output_file + os.path.basename(self.clg_file)
+            fileHandler = open(self.clg_file, "r")  # Open file
+            listOfLines = fileHandler.readlines()  # Get list of all lines in file
+            fileHandler.close()  # Close file
+            first_str = listOfLines.pop(0)
+            update_paras = self.acquire_paras_value()
+            if first_str.rstrip("\n") == '[计算参数]' and self.parameters and update_paras:
+                for line in listOfLines:
+                    line = line.rstrip("\n")
+                    try:
+                        [paras_name, paras_value] = line.split(" = ")
+                        paras_key = get_key(self.paras_map, paras_name)
+                        if paras_key:
+                            self.parameters[paras_key[0]] = paras_value
+                    except Exception as err:
+                        print(err)
+                        continue
+                self.init_paras_callback([self.parameters, self.paras_map])
+            else:
+                print("error")
 
-    # 导入文件窗口
+    # 导出文件
     def export_config(self):
-        self.output_filepath = QFileDialog.getSaveFileName(self, "选择导出的文件夹位置", get_desk_path())
-        if self.output_filepath:
-            pass
+        self.output_filepath, _ = QFileDialog.getSaveFileName(self, "选择导出的文件夹位置", get_desk_path() + "\\parameters",
+                                                              "Text Files (*.txt)")
+        if self.output_filepath and self.paras_map:
+            # 以写的方式打开文件，如果文件不存在，就会自动创建
+            file_write_obj = open(self.output_filepath, 'w')
+            file_write_obj.writelines("[计算参数]")
+            file_write_obj.write('\n')
+            update_paras = self.acquire_paras_value()
+            if self.parameters and update_paras:
+                for key in self.paras_map:
+                    string = self.paras_map[key] + " = " + self.parameters[key]
+                    print(string)
+                    file_write_obj.writelines(string)
+                    file_write_obj.write('\n')
+            file_write_obj.close()
 
     # 开始进行计算
     def handleCalc(self):
         result = self.acquire_paras_value()
-        if result:
-            self.calculate_thread = Calculate(self.parameters)  # 计算进程
-            self.calculate_thread.signal.connect(self.result_panel)
-            self.calculate_thread.start()  # 启动线程
+        if result and self.paras_map:
+            calculate_thread = Calculate(self.parameters)  # 计算进程
+            calculate_thread.signal.connect(self.result_panel)
+            calculate_thread.start()  # 启动线程
             # 多线程保存数据
 
-            self.save_paras_thread = WriteConfig(CONFIG, self.parameters)
-            self.save_paras_thread.signal.connect(self.save_config_callback)
-            self.save_paras_thread.start()  # 启动线程
-        pass
+            save_paras_thread = WriteConfig(CONFIG, self.parameters, self.paras_map)
+            save_paras_thread.signal.connect(self.save_config_callback)
+            save_paras_thread.start()  # 启动线程
+        else:
+            print("无法计算")
 
     def result_panel(self, cal_result):
         """
@@ -69,13 +104,12 @@ class test1(QMainWindow):
         :param cal_result: 计算结果
         :return: None
         """
-        # print(cal_result)
-        self.child = Child(cal_result)  # 创建子窗口实例
-        self.child.exec()
+        child_window = Child(cal_result)  # 创建子窗口实例
+        child_window.exec()
 
     def save_config_callback(self, value):
         if value:
-            print(value)
+            print("保存成功！")
 
     # 从面板中获取所有用户的值
     def acquire_paras_value(self):
@@ -86,6 +120,7 @@ class test1(QMainWindow):
             else:
                 return False
         return True
+
 
 class Child(QDialog):
     """
@@ -115,7 +150,7 @@ class Child(QDialog):
         # 添加数据
         newItem = QTableWidgetItem('表面平均电场')
         self.tableWidget.setItem(0, 0, newItem)
-        surf_ave_e_feild =format(surf_ave_e_feild, '.3e')
+        surf_ave_e_feild = format(surf_ave_e_feild, '.3e')
         newItem1 = QTableWidgetItem(str(surf_ave_e_feild))
         self.tableWidget.setItem(0, 1, newItem1)
         newItem = QTableWidgetItem('自电势系数')
@@ -135,16 +170,56 @@ class Child(QDialog):
                 self.tableWidget.setItem(i + 4, j + 1, newItem)
 
     def save_result(self):
-        self.output_filepath = QFileDialog.getSaveFileName(self, "选择导出的文件夹位置", get_desk_path())
-        if self.output_filepath:
-            pass
+        output_filepath, _ = QFileDialog.getSaveFileName(self, "选择导出的文件夹位置", get_desk_path() + "\\calculate_result",
+                                                         "Text Files (*.txt)")
+        if output_filepath:
+            surf_ave_e_feild = self.args[0]  # 表面平均电场
+            self_pot_coeff = self.args[1]  # 自电势系数
+            q = self.args[2]  # 电荷矩阵
+
+            # 以写的方式打开文件，如果文件不存在，就会自动创建
+            file_write_obj = open(output_filepath, 'w')
+            file_write_obj.writelines("[计算结果]\n")
+            file_write_obj.writelines('表面平均电场\n')
+            surf_ave_e_feild = format(surf_ave_e_feild, '.3e')
+            file_write_obj.writelines(str(surf_ave_e_feild) + '\n')
+            file_write_obj.writelines('自电势系数\n')
+            for i in range(3):
+                format_value = ""
+                for j in range(3):
+                    result_value = format(self_pot_coeff[i][j], '.3e')
+                    format_value = format_value + "   " + str(result_value)
+                file_write_obj.writelines(str(format_value) + '\n')
+
+            file_write_obj.writelines('电荷矩阵\n')
+            for i in range(3):
+                format_value = ""
+                for j in range(1):
+                    result_value = format(q[i][j], '.3e')
+                    format_value = format_value + "   " + str(result_value)
+                file_write_obj.writelines(str(format_value) + '\n')
+            file_write_obj.close()
 
     def quit(self):  # 点击ok是发送内置信号
         self.close()
 
 
 def get_desk_path():
+    """
+    查找桌面路径
+    :return:
+    """
     return os.path.join(os.path.expanduser('~'), "Desktop")
+
+
+def get_key(dict, value):
+    """
+    通过字典的value查找key
+    :param dict:
+    :param value:
+    :return:
+    """
+    return [k for k, v in dict.items() if v == value]
 
 
 if __name__ == '__main__':
